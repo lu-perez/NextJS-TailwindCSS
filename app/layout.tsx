f18e81de5280bef1a8879ef8a90fce764929139b
lu-perez <luperez@teco.com.ar>
2023-04-03 19:43:54 -0300

    isDarkTheme function

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,15 @@ import './globals.css'
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className="dark">
+    <html lang="en" className={isDarkTheme(true)}>
       <head />
-      <body className="dark:bg-black dark:text-white">
+      <body className="dark:bg-gray-900 dark:text-white">
         {children}
       </body>
     </html>
   )
+}
+
+function isDarkTheme(isDark: boolean) {
+  return isDark ? 'dark' : ''
 }
